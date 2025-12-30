db5e37a8b447dec9bf3677cf82bff65996950981
8ertop <alsrwryans459@gmail.com>
2025-12-31 02:51:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,142 +1,62 @@
-import { readFileSync, writeFileSync, existsSync, statSync } from "fs";
-import { spawn, execSync } from "child_process";
-import semver from "semver";
-import axios from "axios";
+const { spawn } = require("child_process");
+const { readFileSync } = require("fs-extra");
+const axios = require("axios");
+const semver = require("semver");
+const logger = require("./utils/log");
+const express = require("express");
+const gradient = require("gradient-string")
+const logo = `
+ ██████╗  █████╗  ███╗   ███╗ ██╗ 
+██╔════╝ ██╔══██╗ ████╗ ████║ ██║ 
+╚█████╗  ██║  ██║ ██╔████╔██║ ██║ 
+ ╚═══██╗ ██║  ██║ ██║╚██╔╝██║ ██║ 
+██████╔╝ ╚█████╔╝ ██║ ╚═╝ ██║ ██║ 
+╚═════╝   ╚════╝  ╚═╝     ╚═╝ ╚═╝ 
+`;
+const c = ["cyan", "#7D053F"];
+const redToGreen = gradient("red", "cyan");
+console.log(redToGreen("━".repeat(50), { interpolation: "hsv" }));
+const text = gradient(c).multiline(logo);
+console.log(text);
+console.log(redToGreen("━".repeat(50), { interpolation: "hsv" }));
 
-import {} from "dotenv/config";
-import logger from "./core/var/modules/logger.js";
-import { loadPlugins } from "./core/var/modules/installDep.js";
 
-import {
-    isGlitch,
-    isReplit,
-    isGitHub,
-} from "./core/var/modules/environments.get.js";
 
-console.clear();
+const app = express();
+const port = process.env.PORT || 3078; 
+app.get("/", (req, res) => {
+  res.send(`Hello im SOMI..🧸`);
+});
 
-// Install newer node version on some old Repls
-function upNodeReplit() {
-    return new Promise((resolve) => {
-        execSync(
-            "npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH"
-        );
-        resolve();
-    });
-}
+function startBot(message) {
+  (message) ? logger(message, "[ Starting ]") : "";
 
-(async () => {
-    if (process.version.slice(1).split(".")[0] < 16) {
-        if (isReplit) {
-            try {
-                logger.warn("Installing Node.js v16 for Repl.it...");
-                await upNodeReplit();
-                if (process.version.slice(1).split(".")[0] < 16)
-                    throw new Error("Failed to install Node.js v16.");
-            } catch (err) {
-                logger.error(err);
-                process.exit(0);
-            }
-        }
-        logger.error(
-            "Xavia requires Node 16 or higher. Please update your version of Node."
-        );
-        process.exit(0);
-    }
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "SOMI.js"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
 
-    if (isGlitch) {
-        const WATCH_FILE = {
-            restart: {
-                include: ["\\.json"],
-            },
-            throttle: 3000,
-        };
+  child.on("close", (codeExit) => {
+    if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+      startBot("Starting up...");
+      global.countRestart += 1;
+      return;
+    } else return;
+  });
 
-        if (
-            !existsSync(process.cwd() + "/watch.json") ||
-            !statSync(process.cwd() + "/watch.json").isFile()
-        ) {
-            logger.warn("Glitch environment detected. Creating watch.json...");
-            writeFileSync(
-                process.cwd() + "/watch.json",
-                JSON.stringify(WATCH_FILE, null, 2)
-            );
-            execSync("refresh");
-        }
-    }
+  child.on("error", function(error) {
+    logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
+  });
+};
 
-    if (isGitHub) {
-        logger.warn("Running on GitHub is not recommended.");
-    }
-})();
 
-// End
+  logger('SOMI BOT', "[ NAME ]");
+  logger("Version: 1.1.0", "[ VERSION ]");
 
-// CHECK UPDATE
-async function checkUpdate() {
-    logger.custom("Checking for updates...", "UPDATE");
-    try {
-        const res = await axios.get(
-            "https://raw.githubusercontent.com/XaviaTeam/XaviaBot/main/package.json"
-        );
 
-        const { version } = res.data;
-        const currentVersion = JSON.parse(
-            readFileSync("./package.json")
-        ).version;
-        if (semver.lt(currentVersion, version)) {
-            logger.warn(`New version available: ${version}`);
-            logger.warn(`Current version: ${currentVersion}`);
-        } else {
-            logger.custom("No updates available.", "UPDATE");
-        }
-    } catch (err) {
-        logger.error("Failed to check for updates.");
-    }
-}
+startBot();
 
-// Child handler
-const _1_MINUTE = 60000;
-let restartCount = 0;
-
-async function main() {
-    await checkUpdate();
-    await loadPlugins();
-    const child = spawn(
-        "node",
-        [
-            "--trace-warnings",
-            "--experimental-import-meta-resolve",
-            "--expose-gc",
-            "core/_build.js",
-        ],
-        {
-            cwd: process.cwd(),
-            stdio: "inherit",
-            env: process.env,
-        }
-    );
-
-    child.on("close", async (code) => {
-        handleRestartCount();
-        if (code !== 0 && restartCount < 5) {
-            console.log();
-            logger.error(`An error occurred with exit code ${code}`);
-            logger.warn("Restarting...");
-            await new Promise((resolve) => setTimeout(resolve, 2000));
-            main();
-        } else {
-            console.log();
-            logger.error("XaviaBot has stopped, press Ctrl + C to exit.");
-        }
-    });
-}
-
-function handleRestartCount() {
-    restartCount++;
-    setTimeout(() => {
-        restartCount--;
-    }, _1_MINUTE);
-}
-
-main();
+app.listen(port, () => {
+  console.log(`bot running in port: ${port}`);
+});
